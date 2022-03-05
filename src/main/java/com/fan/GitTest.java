b8d6df4fada9d6a9db75ff7ab062d70155db6786
fan <1243648205@qq.com>
2022-03-05 10:25:53 +0800

    master second commit

--- src/main/java/com/fan/GitTest.java
+++ src/main/java/com/fan/GitTest.java
@@ -9,5 +9,8 @@ public class GitTest {
         System.out.println("hello word");
         System.out.println("hello word");
         System.out.println("hello word");
+        System.out.println("我很好，只是偶尔被需要");
+        System.out.println("我很好，只是偶尔被需要");
+        System.out.println("我很好，只是偶尔被需要");
     }
 }
