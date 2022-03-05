e954954650bf3e190c600ac29bf5af05977c7d00
fan <1243648205@qq.com>
2022-03-05 10:24:22 +0800

    hot-fix second commit

--- src/main/java/com/fan/GitTest.java
+++ src/main/java/com/fan/GitTest.java
@@ -9,5 +9,10 @@ public class GitTest {
         System.out.println("hello word");
         System.out.println("hello word");
         System.out.println("hello word");
+        System.out.println("hello word");
+        System.out.println("hello word");
+        System.out.println("hello word");
+        System.out.println("hello word");
+        System.out.println("hello word");
     }
 }
