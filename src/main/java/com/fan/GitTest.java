0d1f999a4e18593d001b61b1aaeb665e5268b30c
fan <1243648205@qq.com>
2022-03-05 10:09:58 +0800

    idea first commit

---
+++ src/main/java/com/fan/GitTest.java
@@ -0,0 +1,11 @@
+package com.fan;
+
+/**
+ * @author shkstart
+ * @create2022-03-0510:06
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello word");
+    }
+}
