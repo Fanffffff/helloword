f536364a3eb6a70e822e67cab769ec240b64a2da
Fanffffff <100916358+Fanffffff@users.noreply.github.com>
2022-03-05 10:58:58 +0800

    Update GitTest.java

--- src/main/java/com/fan/GitTest.java
+++ src/main/java/com/fan/GitTest.java
@@ -6,9 +6,7 @@ package com.fan;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello word");
-        System.out.println("hello word");
-        System.out.println("hello word");
+        
         System.out.println("我很好，只是偶尔被需要");
         System.out.println("我很好，只是偶尔被需要");
         System.out.println("我很好，只是偶尔被需要");
