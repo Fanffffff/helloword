f5d5cdb36632d4091119c4f69b9e2517129f399e
fan <1243648205@qq.com>
2022-03-05 10:21:14 +0800

    hot-fix commit

--- src/main/java/com/fan/GitTest.java
+++ src/main/java/com/fan/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello word");
         System.out.println("hello word");
+        System.out.println("hello word");
     }
 }
